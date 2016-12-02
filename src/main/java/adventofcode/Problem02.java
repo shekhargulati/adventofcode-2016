8e1d838cecf6dda04b47db7dc47b22b20c1e4cb5
Shekhar Gulati <shekhargulati84@gmail.com>
2016-12-02 18:29:39 +0530

    Problem 02 refactoring

--- src/main/java/adventofcode/Problem02.java
+++ src/main/java/adventofcode/Problem02.java
@@ -1,17 +1,20 @@
 package adventofcode;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Objects;
+import java.util.Map;
+import java.util.function.Function;
 
 public class Problem02 {
 
-    private static final int[][] arr = {
-            new int[]{1, 2, 3},
-            new int[]{4, 5, 6},
-            new int[]{7, 8, 9}
+    private static final String[][] keypadPart1 = {
+            new String[]{"1", "2", "3"},
+            new String[]{"4", "5", "6"},
+            new String[]{"7", "8", "9"}
     };
 
-    private static final String[][] arr2 = {
+    private static final String[][] keypad_part2 = {
             new String[]{null, null, "1", null, null},
             new String[]{null, "2", "3", "4", null},
             new String[]{"5", "6", "7", "8", "9"},
@@ -19,53 +22,39 @@ public class Problem02 {
             new String[]{null, null, "D", null, null},
     };
 
-    public static String code(List<String> instructions) {
-        StringBuilder codeBuilder = new StringBuilder();
-        Point current = new Point(1, 1);
-        for (String instruction : instructions) {
-            String[] commands = instruction.split("");
-            for (String cmd : commands) {
-                if (Objects.equals(cmd, "U") && current.x > 0) {
-                    current = new Point(current.x - 1, current.y);
-                } else if (Objects.equals(cmd, "D") && current.x < 2) {
-                    current = new Point(current.x + 1, current.y);
-                } else if (Objects.equals(cmd, "L") && current.y > 0) {
-                    current = new Point(current.x, current.y - 1);
-                } else if (Objects.equals(cmd, "R") && current.y < 2) {
-                    current = new Point(current.x, current.y + 1);
-                } else {
-                    // ignore
-                }
-            }
-            codeBuilder.append(arr[current.x][current.y]);
+    private static final Map<String, Function<Point, Point>> mappingsPart1 = new HashMap<String, Function<Point, Point>>() {
+        {
+            put("U", p -> p.x > 0 ? new Point(p.x - 1, p.y) : p);
+            put("D", p -> p.x < keypadPart1.length - 1 ? new Point(p.x + 1, p.y) : p);
+            put("L", p -> p.y > 0 ? new Point(p.x, p.y - 1) : p);
+            put("R", p -> p.y < keypadPart1.length - 1 ? new Point(p.x, p.y + 1) : p);
+        }
+    };
 
+    private static final Map<String, Function<Point, Point>> mappingsPart2 = new HashMap<String, Function<Point, Point>>() {
+        {
+            put("U", p -> (p.x > 0 && keypad_part2[p.x - 1][p.y] != null) ? new Point(p.x - 1, p.y) : p);
+            put("D", p -> (p.x < keypad_part2.length - 1 && keypad_part2[p.x + 1][p.y] != null) ? new Point(p.x + 1, p.y) : p);
+            put("L", p -> (p.y > 0 && keypad_part2[p.x][p.y - 1] != null) ? new Point(p.x, p.y - 1) : p);
+            put("R", p -> (p.y < keypad_part2.length - 1 && keypad_part2[p.x][p.y + 1] != null) ? new Point(p.x, p.y + 1) : p);
+        }
+    };
+
+    public static String code_part1(List<String> instructions) {
+        return code(instructions, keypadPart1, mappingsPart1, new Point(1, 1));
+    }
+
+    public static String code_part2(List<String> instructions) {
+        return code(instructions, keypad_part2, mappingsPart2, new Point(2, 0));
+    }
+
+    private static String code(List<String> instructions, String[][] keypad, Map<String, Function<Point, Point>> mappings, Point current) {
+        StringBuilder codeBuilder = new StringBuilder();
+        for (String instruction : instructions) {
+            current = Arrays.stream(instruction.split("")).reduce(current, (acc, cmd) -> mappings.get(cmd).apply(acc), (p1, p2) -> p1);
+            codeBuilder.append(keypad[current.x][current.y]);
         }
         return codeBuilder.toString();
     }
-
-    public static String code2(List<String> instructions) {
-        StringBuilder codeBuilder = new StringBuilder();
-        Point current = new Point(2, 0);
-        for (String instruction : instructions) {
-            String[] commands = instruction.split("");
-            for (String cmd : commands) {
-                if (Objects.equals(cmd, "U") && (current.x > 0 && arr2[current.x - 1][current.y] != null)) {
-                    current = new Point(current.x - 1, current.y);
-                } else if (Objects.equals(cmd, "D") && (current.x < 4 && arr2[current.x + 1][current.y] != null)) {
-                    current = new Point(current.x + 1, current.y);
-                } else if (Objects.equals(cmd, "L") && (current.y > 0 && arr2[current.x][current.y - 1] != null)) {
-                    current = new Point(current.x, current.y - 1);
-                } else if (Objects.equals(cmd, "R") && (current.y < 4 && arr2[current.x][current.y + 1] != null)) {
-                    current = new Point(current.x, current.y + 1);
-                } else {
-                    // ignore
-                }
-            }
-            codeBuilder.append(arr2[current.x][current.y]);
-
-        }
-        return codeBuilder.toString();
-    }
-
 }
 
--- src/test/java/adventofcode/Problem02Test.java
+++ src/test/java/adventofcode/Problem02Test.java
@@ -14,28 +14,28 @@ public class Problem02Test {
     @Test
     public void should_be_1985() throws Exception {
         List<String> instructions = Arrays.asList("ULL", "RRDDD", "LURDL", "UUUUD");
-        String code = Problem02.code(instructions);
+        String code = Problem02.code_part1(instructions);
         assertThat(code).isEqualTo("1985");
     }
 
     @Test
     public void problem2_input() throws Exception {
         List<String> instructions = Files.readAllLines(Paths.get("src", "test", "resources", "problem02.txt"));
-        String code = Problem02.code(instructions);
+        String code = Problem02.code_part1(instructions);
         assertThat(code).isEqualTo("74921");
     }
 
     @Test
     public void should_be_5DB3_part2() throws Exception {
         List<String> instructions = Arrays.asList("ULL", "RRDDD", "LURDL", "UUUUD");
-        String code = Problem02.code2(instructions);
+        String code = Problem02.code_part2(instructions);
         assertThat(code).isEqualTo("5DB3");
     }
 
     @Test
     public void problem2_input_part2() throws Exception {
         List<String> instructions = Files.readAllLines(Paths.get("src", "test", "resources", "problem02.txt"));
-        String code = Problem02.code2(instructions);
+        String code = Problem02.code_part2(instructions);
         assertThat(code).isEqualTo("A6B35");
     }
 }
