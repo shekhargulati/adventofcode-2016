e3e11ac2f0c5a80d91459b69f5a1bbe0506a8914
Shekhar Gulati <shekhargulati84@gmail.com>
2016-12-03 10:42:45 +0530

    Extracted point to its own class

--- src/main/java/adventofcode/Problem01.java
+++ src/main/java/adventofcode/Problem01.java
@@ -149,39 +149,3 @@ class Position {
     }
 }
 
-class Point {
-    final int x;
-    final int y;
-
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Point point = (Point) o;
-
-        if (x != point.x) return false;
-        return y == point.y;
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = x;
-        result = 31 * result + y;
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "Point{" +
-                "x=" + x +
-                ", y=" + y +
-                '}';
-    }
-}
