8baac6a4143850a14e9a2310a654684df6f75c65
Shekhar Gulati <shekhargulati84@gmail.com>
2016-12-06 21:18:08 +0530

    Day 6 problem refactoring

--- src/main/java/adventofcode/Problem06.java
+++ src/main/java/adventofcode/Problem06.java
@@ -1,16 +1,17 @@
 package adventofcode;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.AbstractMap.SimpleEntry;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.IntStream;
 
-import static java.util.stream.Collectors.joining;
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.*;
 
 /**
  * Read problem statement in src/main/resources/problem-statements/problem06.txt
@@ -25,67 +26,49 @@ public class Problem06 {
         return findCode(inputFilePath, Problem06::min);
     }
 
-    private static String findCode(Path inputFilePath, Function<String, String> converter) throws IOException {
-        Map<Integer, String> map = rowsToColumns(Files.readAllLines(inputFilePath));
+    private static String findCode(Path inputFilePath, Function<String, String> extractor) throws Exception {
+        Map<Integer, String> map = transpose(Files.readAllLines(inputFilePath));
         return map.entrySet()
                 .stream()
-                .map(entry -> new SimpleEntry<>(entry.getKey(), converter.apply(entry.getValue())))
+                .map(entry -> new SimpleEntry<>(entry.getKey(), extractor.apply(entry.getValue())))
                 .sorted((e1, e2) -> e1.getKey() - e2.getKey())
                 .map(SimpleEntry::getValue)
                 .collect(joining());
     }
 
-    private static Map<Integer, String> rowsToColumns(List<String> lines) {
-        Map<Integer, String> columnsWithText = new HashMap<>();
-        lines.forEach(line -> IntStream.range(0, line.length())
-                .forEach(index -> columnsWithText.put(
-                        index,
-                        columnsWithText.compute(
-                                index,
-                                (k, v) -> v == null ?
-                                        String.valueOf(line.charAt(k)) :
-                                        v.concat(String.valueOf(line.charAt(k)))))));
-        return columnsWithText;
+    private static Map<Integer, String> transpose(List<String> lines) {
+        Map<Integer, String> columns = new HashMap<>();
+        lines.forEach(line ->
+                IntStream
+                        .range(0, line.length()).mapToObj(i -> i)
+                        .reduce(columns, (acc, index) -> {
+                            columns.put(
+                                    index,
+                                    columns.compute(
+                                            index,
+                                            (k, v) -> v == null
+                                                    ? String.valueOf(line.charAt(k))
+                                                    : v.concat(String.valueOf(line.charAt(k)))));
+                            return columns;
+                        }, (m1, m2) -> m1));
+        return columns;
     }
 
     private static String max(String input) {
-        String[] chs = input.split("");
-        Map<String, Integer> map = new HashMap<>();
-        for (String ch : chs) {
-            if (map.containsKey(ch)) {
-                map.put(ch, map.get(ch) + 1);
-            } else {
-                map.put(ch, 1);
-            }
-        }
-
-        Map.Entry<String, Integer> max = null;
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            if (max == null || max.getValue() < entry.getValue()) {
-                max = entry;
-            }
-        }
-
-        return max.getKey();
+        Map<String, Long> charWithCount = Arrays.stream(input.split("")).collect(groupingBy(identity(), counting()));
+        return charWithCount.entrySet()
+                .stream()
+                .max((e1, e2) -> Long.valueOf(e1.getValue() - e2.getValue()).intValue())
+                .get()
+                .getKey();
     }
 
     private static String min(String input) {
-        String[] chs = input.split("");
-        Map<String, Integer> map = new HashMap<>();
-        for (String ch : chs) {
-            if (map.containsKey(ch)) {
-                map.put(ch, map.get(ch) + 1);
-            } else {
-                map.put(ch, 1);
-            }
-        }
-
-        Map.Entry<String, Integer> min = null;
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            if (min == null || entry.getValue() < min.getValue()) {
-                min = entry;
-            }
-        }
-        return min.getKey();
+        Map<String, Long> charWithCount = Arrays.stream(input.split("")).collect(groupingBy(identity(), counting()));
+        return charWithCount.entrySet()
+                .stream()
+                .min((e1, e2) -> Long.valueOf(e1.getValue() - e2.getValue()).intValue())
+                .get()
+                .getKey();
     }
 }
