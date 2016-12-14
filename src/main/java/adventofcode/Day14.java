338c95b6951f1bf1ce2a173df45acfd1e7cecc8c
Shekhar Gulati <shekhargulati84@gmail.com>
2016-12-14 23:01:23 +0530

    Day 14 part 2 working

--- src/main/java/adventofcode/Day14.java
+++ src/main/java/adventofcode/Day14.java
@@ -1,32 +1,59 @@
 package adventofcode;
 
-import java.util.AbstractMap;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.LongStream;
+
+import static adventofcode.Utils.md5;
 
 public class Day14 {
 
     public static void main(String[] args) {
 
         String salt = "ahsbgdzn";
+        Map<Long, String> cache = new HashMap<>();
         LongStream.iterate(0, i -> i + 1)
-                .mapToObj(i -> new AbstractMap.SimpleEntry<>(Utils.md5(salt + i).toLowerCase(), i))
+                .mapToObj(i -> new SimpleEntry<>(md5(salt + i).toLowerCase(), i))
+                .map(e -> {
+                    if (cache.containsKey(e.getValue())) {
+                        return new SimpleEntry<>(cache.get(e.getValue()), e.getValue());
+                    }
+                    String hash = e.getKey();
+                    for (int i = 0; i < 2016; i++) {
+                        hash = md5(hash).toLowerCase();
+                    }
+                    cache.putIfAbsent(e.getValue(), hash);
+                    return new SimpleEntry<>(hash, e.getValue());
+                })
                 .filter(entry -> hasThreeSameCharacters(entry.getKey()) != null)
-                .map(entry -> new AbstractMap.SimpleEntry<>(hasThreeSameCharacters(entry.getKey()), entry))
-                .filter(entry -> {
-                    Optional<AbstractMap.SimpleEntry<String, Long>> first = LongStream
-                            .rangeClosed(entry.getValue().getValue() + 1, entry.getValue().getValue() + 1000)
-                            .mapToObj(i -> new AbstractMap.SimpleEntry<>(Utils.md5(salt + i).toLowerCase(), i))
-                            .filter(e -> hasSameCharactersFiveTimes(e.getKey(), entry.getKey()) != null)
+                .map(entry -> new SimpleEntry<>(hasThreeSameCharacters(entry.getKey()), entry))
+                .filter(entryOfEntry -> {
+                    Optional<SimpleEntry<String, Long>> first = LongStream
+                            .rangeClosed(entryOfEntry.getValue().getValue() + 1, entryOfEntry.getValue().getValue() + 1000)
+                            .mapToObj(i -> new SimpleEntry<>(md5(salt + i).toLowerCase(), i))
+                            .map(e -> {
+                                if (cache.containsKey(e.getValue())) {
+                                    return new SimpleEntry<>(cache.get(e.getValue()), e.getValue());
+                                }
+                                String hash = e.getKey();
+                                for (int i = 0; i < 2016; i++) {
+                                    hash = md5(hash).toLowerCase();
+                                }
+                                cache.putIfAbsent(e.getValue(), hash);
+                                return new SimpleEntry<>(hash, e.getValue());
+                            })
+                            .filter(e -> hasSameCharactersFiveTimes(e.getKey(), entryOfEntry.getKey()) != null)
                             .findFirst();
-                    if(first.isPresent()){
+                    if (first.isPresent()) {
                         System.out.println(first.get());
                     }
                     return first.isPresent();
                 })
                 .limit(64)
-                .forEach(e ->{
+                .forEach(e -> {
                     System.out.println(e);
                     System.out.println();
                 });
