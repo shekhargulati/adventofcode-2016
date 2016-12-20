7e5b1ee1e0ab76b0675ddcfce6ebb43bf59010a4
Shekhar Gulati <shekhargulati84@gmail.com>
2016-12-20 09:47:40 +0530

    Day 19 part1 circlular buffer

--- src/main/java/adventofcode/Day19.java
+++ src/main/java/adventofcode/Day19.java
@@ -1,29 +1,30 @@
 package adventofcode;
 
 import java.util.Arrays;
+import java.util.Iterator;
 
 public class Day19 {
 
     public static void main(String[] args) throws Exception {
-
-        Elf[] elfs = new Elf[3012210];
-        for (int i = 0; i < elfs.length; i++) {
-            elfs[i] = new Elf(i + 1, 1);
+        CircularBuffer elfs = new CircularBuffer(3012210);
+        for (int i = 0; i < elfs.size(); i++) {
+            elfs.add(new Elf(i + 1, 1));
         }
-        while (elfs.length > 1) {
-            System.out.println(elfs.length);
-            for (int i = 0; i < elfs.length; i++) {
-                if(elfs[i].presents > 0){
-                    int next = i + 1 < elfs.length ? i + 1 : 0;
-                    elfs[i].addPresent(elfs[next].presents);
-                    elfs[next].zero();
+
+        while (elfs.size() > 1) {
+            for (int i = 0; i < elfs.size(); i++) {
+                Elf current = elfs.get(i);
+                if (current.presents > 0) {
+                    Elf left = elfs.get(i + 1);
+                    current.addPresents(left.presents);
+                    left.zero();
                 }
             }
-            elfs = Arrays.stream(elfs).filter(e -> e.presents > 0).toArray(Elf[]::new);
-//            System.out.println(elfs.length);
+
+            elfs = elfs.filterZero();
         }
 
-        System.out.println(Arrays.toString(elfs));
+        System.out.println(elfs.get(0));
 
     }
 }
@@ -37,7 +38,7 @@ class Elf {
         this.presents = presents;
     }
 
-    public void addPresent(int p) {
+    public void addPresents(int p) {
         this.presents += p;
     }
 
@@ -53,3 +54,77 @@ class Elf {
                 '}';
     }
 }
+
+
+class CircularBuffer implements Iterable<Elf> {
+    private final int n;
+    private final Elf[] items;
+    private int currentLocation;
+
+    public CircularBuffer(int n) {
+        this.n = n;
+        this.items = new Elf[n];
+    }
+
+    public void add(Elf item) {
+        items[currentLocation++] = item;
+    }
+
+    public Elf get(int i) {
+        if (i == items.length) {
+            i = 0;
+        }
+        return items[i];
+    }
+
+    public int size() {
+        return this.n;
+    }
+
+    public CircularBuffer filterZero() {
+        int nonZero = 0;
+        for (int i = 0; i < this.size(); i++) {
+            if (this.get(i).presents > 0) {
+                nonZero++;
+            }
+        }
+
+        CircularBuffer copy = new CircularBuffer(nonZero);
+        for (int i = 0; i < this.size(); i++) {
+            if (this.get(i).presents > 0) {
+                copy.add(this.get(i));
+            }
+        }
+        return copy;
+    }
+
+    @Override
+    public Iterator<Elf> iterator() {
+        return new CircularBufferIterator(items);
+    }
+
+    private class CircularBufferIterator implements Iterator<Elf> {
+
+        private final Elf[] copy;
+        int index;
+
+        public CircularBufferIterator(Elf[] items) {
+            this.copy = Arrays.copyOf(items, items.length);
+        }
+
+        @Override
+        public boolean hasNext() {
+            return index < items.length;
+        }
+
+        @Override
+        public Elf next() {
+            if (items[index].presents > 0) {
+                return items[index++];
+            }
+            index++;
+            return next();
+        }
+    }
+}
+
